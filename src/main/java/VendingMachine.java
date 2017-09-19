4acde1bed26403a92184832d727b1a2b7e7ee1ee
gdhillon7731 <dhillon7731@gmail.com>
2017-09-19 13:05:09 -0400

    Test: Invalid Coins (Penny)

---
+++ src/main/java/VendingMachine.java
@@ -0,0 +1,8 @@
+
+public class VendingMachine {
+
+
+    public static Boolean isValidCoin(String coin){
+        return false;
+    }
+}
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,13 +1,16 @@
 
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTest {
 
-    @Test
-    public void test(){
 
-            test();
+    @Test
+    public void penny_Invalid_Coin_Identifies(){
+        VendingMachine vendingMachine = new VendingMachine();
+        Boolean invalidCoin = vendingMachine.isValidCoin("penny");
+        assertEquals("penny", invalidCoin);
     }
 
 
